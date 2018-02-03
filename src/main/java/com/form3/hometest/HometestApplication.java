02b0ea363ccb35bc1217832523c2f8aa79ae94ba
Hathazy Laszlo <hathazy.laszlo@gmail.com>
2018-02-03 23:03:58 +0200

    Initial commit

---
+++ src/main/java/com/form3/hometest/HometestApplication.java
@@ -0,0 +1,12 @@
+package com.form3.hometest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HometestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HometestApplication.class, args);
+	}
+}
