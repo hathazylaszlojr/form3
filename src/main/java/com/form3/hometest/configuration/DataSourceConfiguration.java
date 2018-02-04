cbb4916368ba7d9f96efc6ad8ba403874f0f0c9e
Hathazy Laszlo <hathazy.laszlo@gmail.com>
2018-02-04 16:45:31 +0200

    Add hsql persistence configuration

---
+++ src/main/java/com/form3/hometest/configuration/DataSourceConfiguration.java
@@ -0,0 +1,37 @@
+package com.form3.hometest.configuration;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfiguration {
+
+    @Value("${db.endpoint}")
+    private String dbEndpoint;
+
+    @PostConstruct
+    public void initializeDatabase() {
+        final Flyway flyway = new Flyway();
+        flyway.setDataSource(dataSource());
+        flyway.migrate();
+    }
+
+    @Bean("dataSource")
+    public DataSource dataSource() { return createDatasource(); }
+
+    private DataSource createDatasource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
+        dataSource.setUrl(dbEndpoint);
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+
+}
---
+++ src/main/java/com/form3/hometest/configuration/PersistenceConfiguration.java
@@ -0,0 +1,29 @@
+package com.form3.hometest.configuration;
+
+import com.form3.hometest.entity.Account;
+import com.form3.hometest.entity.Payment;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+public class PersistenceConfiguration {
+
+    @Bean
+    public SessionFactory sessionFactory(@Qualifier("dataSource") DataSource dataSource) {
+        LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
+
+        sessionBuilder.addAnnotatedClasses(Account.class);
+        sessionBuilder.addAnnotatedClasses(Payment.class);
+
+        return sessionBuilder.buildSessionFactory();
+    }
+
+
+}
